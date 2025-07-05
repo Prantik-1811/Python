b2669e2dac55f55dedec5f8f593e95356a843969
Prantik-1811 <prantik.chandra05@gmail.com>
2025-07-05 04:23:51 +0000

    commit

---
+++ Python_programs/program2.py
@@ -0,0 +1,9 @@
+# This program deals with the usage, declaration and assignment of variables
+
+message = "Hello Prantik"
+print(message)
+
+#changing value of the variable message
+
+message = "Hi Prantik"
+print(message)
